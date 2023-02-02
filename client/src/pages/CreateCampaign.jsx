c997919fddfaa5902c809d27c656f62fbfb9ba9d
z-elmasry <123253418+z-elmasry@users.noreply.github.com>
2023-02-02 14:24:27 +0200

    changed the inputType of 'Goal' to number

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -82,7 +82,7 @@ const CreateCampaign = () => {
           <FormField 
             labelName="Goal *"
             placeholder="ETH 0.50"
-            inputType="text"
+            inputType="number"
             value={form.target}
             handleChange={(e) => handleFormFieldChange('target', e)}
           />
